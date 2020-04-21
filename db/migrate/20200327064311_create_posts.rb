039c859c138d82a19ae3c0661106b2c2321b58c5
yosukekoshoji <58423182+yosukekoshoji@users.noreply.github.com>
2020-04-21 23:19:24 +0900

    error
    本番環境エラー修正（全角スペース除去）

--- db/migrate/20200327064311_create_posts.rb
+++ db/migrate/20200327064311_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.text :post　,null: false
+      t.text :post,null: false
       t.integer :user_id ,null: false
       t.timestamps
     end
